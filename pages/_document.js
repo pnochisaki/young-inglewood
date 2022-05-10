680fc5ba2edf6b23ebe2ae243e7818593decc915
pnochisaki <pnochisaki@gmail.com>
2022-05-10 14:50:47 -0700

    remove favicons

--- pages/_document.js
+++ pages/_document.js
@@ -13,15 +13,7 @@ class MyDocument extends Document {
 				<Head>
         <link rel="stylesheet" href="https://use.typekit.net/hky1kqm.css" />
         <meta name='description' content='Young Inglewood' />
-        <link rel="apple-touch-icon" sizes="120x120" href="/favicons/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-        <link rel="manifest" href="/favicons/site.webmanifest" />
-        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-        <link rel="shortcut icon" href="/favicons/favicon.ico" />
-        <meta name="msapplication-TileColor" content="#2d89ef" />
-        <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
-        <meta name="theme-color" content="#ffffff" />        
+
 				</Head>
         <body>
           <Main />
