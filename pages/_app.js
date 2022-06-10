ea4125ce3ef62d79d21ce4850ffcbfeb94a8a78b
pnochisaki <pnochisaki@gmail.com>
2022-06-10 13:03:04 -0700

    some commerce 7 stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Component {...pageProps} />
+    <Script
+      src="https://cdn.commerce7.com/v2/commerce7.js"
+      id="c7-javascript"
+      data-tenant="young-inglewood-vineyards"
+    />
+  </>
 }
 
 export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -10,17 +10,16 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-				<Head>
-        <link rel="stylesheet" href="https://use.typekit.net/hky1kqm.css" />
-        <meta name='description' content='Young Inglewood' />
-        <link href="https://cdn.commerce7.com/v2/commerce7.css" rel="stylesheet" />
+        <Head>
+          <link rel="stylesheet" href="https://use.typekit.net/hky1kqm.css" />
+          <meta name='description' content='Young Inglewood' />
+          <link href="https://cdn.commerce7.com/v2/commerce7.css" rel="stylesheet" />
 
 
-				</Head>
+        </Head>
         <body>
           <Main />
           <NextScript />
-          <script type="text/javascript" src="https://cdn.commerce7.com/v2/commerce7.js" id="c7-javascript" data-tenant="young-inglewood-vineyards"></script>
         </body>
       </Html>
     )
