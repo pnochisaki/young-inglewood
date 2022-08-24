60f76240cfc660cfe74931ea8d93e0bed66a6b4b
pnochisaki <pnochisaki@gmail.com>
2022-08-24 10:13:13 -0700

    overlay bug

--- components/layout.js
+++ components/layout.js
@@ -8,31 +8,12 @@ import { useEffect, useState, useContext } from 'react'
 
 import { useRouter } from 'next/router';
 
-const userPass = Buffer.from(process.env.C7_USER + ":" + process.env.C7_PASS).toString('base64');
-
-// This gets called on every request
-export async function getServerSideProps() {
-  // Fetch data from external API
-  const res = await fetch('https://api.commerce7.com/v1/collection', {
-    method: 'get',
-    headers: new Headers({
-      'Authorization': 'Basic ' + userPass,
-      'Content-Type': 'application/json',
-      'tenant': 'young-inglewood-vineyards'
-    })
-  });
-  const data = await res.json()
-
-  // Pass data to the page via props
-  return { props: { data } }
-}
-
-
-export default function Layout({ data, home, discover, wine, purchase, taste, membership, faq, contact, join, account, credits, children, props }) {
+export default function Layout({ home, discover, wine, purchase, taste, membership, faq, contact, join, account, credits, children, props }) {
 
   const router = useRouter()
 
   const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
   const hamburgerClick = (e) => {
     if (!hamburgerOpen) {
       setHamburgerOpen(true)
@@ -40,6 +21,23 @@ export default function Layout({ data, home, discover, wine, purchase, taste, me
       setHamburgerOpen(false)
     }
   }
+  const [collections, setCollections] = useState(false);
+
+  const fetchCollections = () => {
+    fetch('/api/collections')
+      .then(response => response.json())
+      .then(data => {
+        setCollections(data.collections)
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+  useEffect(() => {
+    fetchCollections();
+  }, [])
+
 
   const text = "Young Inglewood is committed to producing wines of place, stewarded from vine to bottle by mother and son winemakers Jacky and Scott. With old world non-interventionist winemaking practices, our estate’s signature character is transformed into wines of balance and grace."
 
@@ -98,13 +96,13 @@ export default function Layout({ data, home, discover, wine, purchase, taste, me
           <div className="subnavigation">
             <div className="collections-nav">
               {console.log(router.asPath)}
-              {console.log("data", data)}
-              {/* {data.collections
+              {console.log("data collections: ", collections)}
+              {collections && collections
                 .filter(collection => collection.metaData['store-menu'])
                 .map((collection, index) => {
                   return <a className={router.asPath === '/collection/' + collection.slug ? 'c7-btn active' : 'c7-btn'} key={index} href={'/collection/' + collection.slug}><span>{collection.title}</span></a>
                 }
-                )} */}
+                )}
             </div>
           </div>
 
--- pages/collection/[id].js
+++ pages/collection/[id].js
@@ -1,42 +1,10 @@
 import Layout from '../../components/layout';
-import { useRouter } from 'next/router';
 
-const userPass = Buffer.from(process.env.C7_USER + ":" + process.env.C7_PASS).toString('base64');
-
-// This gets called on every request
-export async function getServerSideProps() {
-  // Fetch data from external API
-  const res = await fetch('https://api.commerce7.com/v1/collection', {
-    method: 'get',
-    headers: new Headers({
-      'Authorization': 'Basic ' + userPass,
-      'Content-Type': 'application/json',
-      'tenant': 'young-inglewood-vineyards'
-    })
-  });
-  const data = await res.json()
-
-  // Pass data to the page via props
-  return { props: { data } }
-}
-
-export default function Collection({ data }) {
+export default function Collection() {
   
-  const router = useRouter()
-
   return (
     <Layout purchase>
       <div className="page-margins">
-        <div className="collections-nav">
-          {console.log(router.asPath)}
-          {/* {console.log(data.collections)} */}
-          {data.collections
-            .filter(collection => collection.metaData['store-menu'])
-            .map((collection, index) => {
-              return <a className={router.asPath === '/collection/' + collection.slug ? 'c7-btn active' : 'c7-btn'} key={index} href={'/collection/' + collection.slug}><span>{collection.title}</span></a>
-            }
-            )}
-        </div>
         <div id="c7-content"></div>
       </div>
     </Layout>
