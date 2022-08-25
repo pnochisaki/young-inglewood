107545d445943b7f51e5cf040586e41afdb9e4c3
pnochisaki <pnochisaki@gmail.com>
2022-08-25 16:24:32 -0700

    script

--- pages/_document.js
+++ pages/_document.js
@@ -14,12 +14,11 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Script
+        <script
           strategy="beforeInteractive"
           src="https://cdn.commerce7.com/v2/commerce7.js"
           id="c7-javascript"
-          data-tenant="young-inglewood-vineyards">
-        </Script>
+          data-tenant="young-inglewood-vineyards" />
       </body>
     </Html>
   )
