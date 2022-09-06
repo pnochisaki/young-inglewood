f48a6af7c1c4f269efe90dd40ffc4c00d389cc27
pnochisaki <pnochisaki@gmail.com>
2022-09-06 15:23:21 -0700

    order-history

---
+++ pages/profile/order-history/[id].js
@@ -0,0 +1,13 @@
+import Layout from '../../../components/layout';
+
+
+export default function History() {
+
+  return (
+    <Layout account>
+      <div id="c7-content"></div>
+    </Layout>
+  )
+
+}
+
