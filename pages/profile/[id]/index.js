7376f3017863bb71a34cf306d07341c8f3edb0dd
pnochisaki <pnochisaki@gmail.com>
2022-09-06 15:25:33 -0700

    glogbal

---
+++ pages/profile/[id]/index.js
@@ -0,0 +1,12 @@
+import Layout from '../../components/layout'
+export default function Profile() {
+  return (
+    <>
+      <Layout account>
+        <div className="profile">
+          <div id="c7-content"></div>
+        </div>
+      </Layout>
+    </>
+  )
+}
