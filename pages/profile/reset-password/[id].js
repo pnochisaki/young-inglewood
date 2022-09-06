8b0c8afe143d6948e6300b91133fba4f9e8e8f6f
pnochisaki <pnochisaki@gmail.com>
2022-09-06 09:48:34 -0700

    reset

---
+++ pages/profile/reset-password/[id].js
@@ -0,0 +1,13 @@
+import Layout from '../../../components/layout';
+
+
+export default function Reset() {
+
+  return (
+    <Layout account>
+      <div id="c7-content"></div>
+    </Layout>
+  )
+
+}
+
