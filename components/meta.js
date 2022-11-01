e812b653f0d553c2e0bd023b7cfc42b6436e4c5a
pnochisaki <pnochisaki@gmail.com>
2022-11-01 12:53:55 -0700

    url fix

--- components/meta.js
+++ components/meta.js
@@ -6,7 +6,7 @@ export default function Meta(props) {
       <Head>
       <title>{props.data.title}</title>
       <meta name="description" content={props.data.description} />
-      <meta property="og:image" content={"https://www.younginglewood.com/" + props.data.og_image} />
+      <meta property="og:image" content={"https://www.younginglewood.com" + props.data.og_image} />
       </Head>
     </>
   )
