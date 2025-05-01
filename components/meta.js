1497e5de56eb5706bf45d040327a9cbbb658add1
Peter <peter@510interactive.com>
2025-04-30 21:17:18 -0700

    deploy

--- components/meta.js
+++ components/meta.js
@@ -1,12 +1,14 @@
 import Head from 'next/head'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function Meta(props) {
   return (
     <>
+      <GoogleTagManager gtmId="GTM-TFFRHCGB" />
       <Head>
-      <title>{props.data.title}</title>
-      <meta name="description" content={props.data.description} />
-      <meta property="og:image" content={"https://www.younginglewood.com" + props.data.og_image} />
+        <title>{props.data.title}</title>
+        <meta name="description" content={props.data.description} />
+        <meta property="og:image" content={"https://www.younginglewood.com" + props.data.og_image} />
       </Head>
     </>
   )
