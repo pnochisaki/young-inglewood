5a1df7d7a744dbfb42f2b35383cc6fc512cb9cfd
Peter <peter@510interactive.com>
2025-05-01 16:37:41 -0700

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import '../fonts/fonts.css'
 import $ from 'jquery'
 import { useEffect } from 'react'
 import Head from 'next/head'
-import Script from 'next/script'
 import Router from 'next/router'
 
 
@@ -41,18 +40,6 @@ function MyApp({ Component, pageProps }) {
     <Head>
       <title>Young Inglewood</title>
     </Head>
-    <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-
-    <Script strategy="lazyOnload">
-      {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-        page_path: window.location.pathname,
-        });
-    `}
-    </Script>
     <Component {...pageProps} />
     {/* <Script
       strategy="beforeInteractive"
