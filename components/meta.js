cd9b6fdedf3c32b004cf6e2ba07988bb3ba7f576
Peter <peter@510interactive.com>
2025-06-10 08:25:42 -0700

    google site verification

--- components/meta.js
+++ components/meta.js
@@ -7,6 +7,7 @@ export default function Meta(props) {
       <GoogleTagManager gtmId="GTM-TFFRHCGB" />
       <Head>
         <title>{props.data.title}</title>
+        <meta name="google-site-verification" content="NNmi-88pWy8gKwkG7yajuZ__mdboUCfC6s9R8eN35JM" />
         <meta name="description" content={props.data.description} />
         <meta property="og:image" content={"https://www.younginglewood.com" + props.data.og_image} />
       </Head>
