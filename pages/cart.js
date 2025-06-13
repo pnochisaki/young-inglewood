7ac967fd4cb21620e26a556dd9132ceca0e14793
Peter <peter@510interactive.com>
2025-06-12 19:15:59 -0700

    cart GTM

--- pages/cart.js
+++ pages/cart.js
@@ -1,4 +1,5 @@
 import Layout from '../components/layout'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function Cart() {
   return (
@@ -7,6 +8,7 @@ export default function Cart() {
         <div className="cart">
           <div className="page-margins">
             <div id="c7-content"></div>
+            <GoogleTagManager gtmId="GTM-TFFRHCGB" />
           </div>
         </div>
       </>
