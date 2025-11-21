7f2cf85a536452de78fd93bb88b48187575f8ca6
Peter <peter@510interactive.com>
2025-11-21 14:48:33 -0800

    upload to test

--- components/layout.js
+++ components/layout.js
@@ -58,11 +58,12 @@ export default function Layout({ home, discover, wine, purchase, visit, membersh
   const fetchCart = () => {
     fetch('/api/cart')
       .then(response => {
+        console.log("Cart Response: ", response)
         // On success, re-fetch the cart data to get the latest state
-        getCartData().then(cartData => {
-          // Re-render your custom cart UI with the fresh data
-          renderCart(cartData);
-        });
+        // getCartData().then(cartData => {
+        //   // Re-render your custom cart UI with the fresh data
+        //   renderCart(cartData);
+        // });
       });
   }
 
@@ -89,20 +90,20 @@ export default function Layout({ home, discover, wine, purchase, visit, membersh
       return getCookie(cookieName)
     }
 
-    setInterval(() => {
-      const checkCookie = isThereCookie('customerToken')
-      if (checkCookie) {
-        setLoggedIn(true)
-        $('.layout').addClass('logged-in').removeClass('logged-out')
-        if ($('.account-nav.mobile-only.logged-in a.active').length === 0) { console.log("ADD THE CLASS"); $('.account-nav.mobile-only.logged-in a:first-of-type').addClass('active') }
-        fetchCart();
-        console.log("logged in")
-      } else if (!checkCookie) {
-        setLoggedIn(false)
-        $('.layout').addClass('logged-out').removeClass('logged-in')
-        fetchCart();
-      }
-    }, 500)
+    // setInterval(() => {
+    //   const checkCookie = isThereCookie('customerToken')
+    //   if (checkCookie) {
+    //     setLoggedIn(true)
+    //     $('.layout').addClass('logged-in').removeClass('logged-out')
+    //     if ($('.account-nav.mobile-only.logged-in a.active').length === 0) { console.log("ADD THE CLASS"); $('.account-nav.mobile-only.logged-in a:first-of-type').addClass('active') }
+    //     fetchCart();
+    //     console.log("logged in")
+    //   } else if (!checkCookie) {
+    //     setLoggedIn(false)
+    //     $('.layout').addClass('logged-out').removeClass('logged-in')
+    //     fetchCart();
+    //   }
+    // }, 500)
 
   }, [])
 
---
+++ pages/api/cart.js
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+const userPass = Buffer.from(process.env.C7_USER + ":" + process.env.C7_PASS).toString('base64');
+
+export default async function fetchClubs(req, res) {
+
+  const clubs = await fetch('https://api.commerce7.com/v1/cart', {
+    method: 'get',
+    headers: new Headers({
+      'Authorization': 'Basic ' + userPass,
+      'Content-Type': 'application/json',
+      'tenant': 'young-inglewood-vineyards'
+    })
+  });
+
+  const cartResponse = await cart.json()
+  res.status(200).json(cartResponse)
+}
+
