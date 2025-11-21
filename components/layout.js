939872a83e83935f7f3d21ce490b571f685c9847
Peter <peter@510interactive.com>
2025-11-21 15:03:47 -0800

    routerAsPAth

--- components/layout.js
+++ components/layout.js
@@ -97,8 +97,12 @@ export default function Layout({ home, discover, wine, purchase, visit, membersh
         setLoggedIn(true)
         $('.layout').addClass('logged-in').removeClass('logged-out')
         if ($('.account-nav.mobile-only.logged-in a.active').length === 0) { console.log("ADD THE CLASS"); $('.account-nav.mobile-only.logged-in a:first-of-type').addClass('active') }
-        clearInterval();
+        
         console.log("logged in")
+        console.log("router.asPath:" router.asPath)
+        // window.location.href = '/profile'
+        clearInterval();
+
       } else if (!checkCookie) {
         setLoggedIn(false)
         $('.layout').addClass('logged-out').removeClass('logged-in')
