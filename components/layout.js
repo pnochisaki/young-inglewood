931e15c36100569a6e226ca7c33b281ad5eba377
Peter <peter@510interactive.com>
2025-11-21 16:24:55 -0800

    cachebust

--- components/layout.js
+++ components/layout.js
@@ -105,7 +105,7 @@ export default function Layout({ home, discover, wine, purchase, visit, membersh
         if ($('.account-nav.mobile-only.logged-in a.active').length === 0) { console.log("ADD THE CLASS"); $('.account-nav.mobile-only.logged-in a:first-of-type').addClass('active') }
 
         if (isRedirect === 'true') {
-            window.location.href = '/profile'
+            window.location.href = '/profile?cachebust=true'
         } else {
           if (window.location.pathname === '/profile/login') {
             window.location.href = '/profile?redirect=true'
