9ee8e6808dfa570eef62bebea39d041bd01842eb
Peter <peter@510interactive.com>
2025-11-22 15:44:59 -0800

    desktop dash nav lightup

--- components/layout.js
+++ components/layout.js
@@ -103,6 +103,7 @@ export default function Layout({ home, discover, wine, purchase, visit, membersh
         setLoggedIn(true)
         $('.layout').addClass('logged-in').removeClass('logged-out')
         if ($('.account-nav.mobile-only.logged-in a.active').length === 0) { console.log("ADD THE CLASS"); $('.account-nav.mobile-only.logged-in a:first-of-type').addClass('active') }
+        if ($('.account-nav.desktop-only.logged-in a.active').length === 0) { console.log("ADD THE CLASS"); $('.account-nav.desktop-only.logged-in a:first-of-type').addClass('active') }
 
         if (isRedirect === 'true') {
             window.location.href = '/profile?cachebust=true'
