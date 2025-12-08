8e3ed62cb809604e9b1aad06a029542479f9799b
Peter <peter@510interactive.com>
2025-12-08 00:15:14 -0800

    put gtm back on collections

--- pages/collection/[id].js
+++ pages/collection/[id].js
@@ -2,12 +2,12 @@ import Layout from '../../components/layout';
 import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function Collection() {
-  
+
   return (
     <Layout purchase>
       <div className="page-margins">
         <div id="c7-content"></div>
-      {/* <GoogleTagManager gtmId="GTM-TFFRHCGB" /> */}
+        <GoogleTagManager gtmId="GTM-TFFRHCGB" />
       </div>
     </Layout>
   )
