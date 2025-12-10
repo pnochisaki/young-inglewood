2a213ad0a4345993d644d709b33317d1ef54be07
Peter <peter@510interactive.com>
2025-12-10 12:39:07 -0800

    go up on collections

--- pages/collection/[id].js
+++ pages/collection/[id].js
@@ -1,22 +1,22 @@
 import C7content from '../../components/c7content';
 import Layout from '../../components/layout';
 import { GoogleTagManager } from '@next/third-parties/google'
-import Script from 'next/script';
+// import { useEffect } from 'react';
 
 export default function Collection() {
 
-    useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://cdn.commerce7.com/v2/commerce7.js';
-    script.dataset.tenant = 'young-inglewood-vineyards';
-    script.id = 'c7-javascript';
-    script.async = true;
-    document.body.appendChild(script);
+  //   useEffect(() => {
+  //   const script = document.createElement('script');
+  //   script.src = 'https://cdn.commerce7.com/v2/commerce7.js';
+  //   script.dataset.tenant = 'young-inglewood-vineyards';
+  //   script.id = 'c7-javascript';
+  //   script.async = true;
+  //   document.body.appendChild(script);
 
-    return () => {
-      // Clean up if necessary
-    };
-  }, []);
+  //   return () => {
+  //     // Clean up if necessary
+  //   };
+  // }, []);
 
 
   return (
