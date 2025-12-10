283296908848663be589358f32e105acf6efea0b
Peter <peter@510interactive.com>
2025-12-10 12:47:52 -0800

    useEffect in the component, not the route

--- components/c7content.js
+++ components/c7content.js
@@ -1,4 +1,18 @@
 export default function C7content(props) {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://cdn.commerce7.com/v2/commerce7.js';
+        script.dataset.tenant = 'young-inglewood-vineyards';
+        script.id = 'c7-javascript';
+        script.async = true;
+        document.body.appendChild(script);
+
+        return () => {
+            // Clean up if necessary
+        };
+    }, []);
+
+
     return (
         <div id="c7-content"></div>
     )
--- pages/collection/[id].js
+++ pages/collection/[id].js
@@ -5,20 +5,6 @@ import { useEffect } from 'react';
 
 export default function Collection() {
 
-    useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://cdn.commerce7.com/v2/commerce7.js';
-    script.dataset.tenant = 'young-inglewood-vineyards';
-    script.id = 'c7-javascript';
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      // Clean up if necessary
-    };
-  }, []);
-
-
   return (
     <Layout purchase>
       <div className="page-margins">
