eca26adab1d569387e458c35c2e6ab673cd507ca
Peter <peter@510interactive.com>
2025-12-09 23:00:06 -0800

    GTM back and cache bust c7 script

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="stylesheet" href="https://use.typekit.net/hky1kqm.css" />
-        <link href="https://cdn.commerce7.com/v2/commerce7.css" rel="stylesheet" />
+        <link href="https://cdn.commerce7.com/v2/commerce7.css?v=20251209" rel="stylesheet" />
       </Head>
 
       <body>
@@ -15,7 +15,7 @@ export default function Document() {
         <NextScript />
         <script
           strategy="beforeInteractive"
-          src="https://cdn.commerce7.com/v2/commerce7.js"
+          src="https://cdn.commerce7.com/v2/commerce7.js?v=20251209"
           id="c7-javascript"
           data-tenant="young-inglewood-vineyards" />
       </body>
