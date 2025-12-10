94b0bc80c31367ae1acc497d1b2fac71a0b399cc
Peter <peter@510interactive.com>
2025-12-10 02:04:58 -0800

    undo

--- pages/collection/[id].js
+++ pages/collection/[id].js
@@ -1,10 +1,9 @@
 import Layout from '../../components/layout';
 import { GoogleTagManager } from '@next/third-parties/google'
-import { unstable_noStore } from 'next/cache';
 
 export default function Collection() {
+  
   return (
-    unstable_noStore();
     <Layout purchase>
       <div className="page-margins">
         <div id="c7-content"></div>
