25f70550d95a91d8776c33d54471a9ce14c17a3e
Peter <peter@510interactive.com>
2025-12-10 12:57:03 -0800

    afterInteractive

--- pages/_app.js
+++ pages/_app.js
@@ -45,12 +45,12 @@ function MyApp({ Component, pageProps }) {
       <title>Young Inglewood</title>
     </Head>
     <Component {...pageProps} />
-    {/* <Script
-      strategy="beforeInteractive"
+    <Script
+      strategy="afterInteractive"
       src="https://cdn.commerce7.com/v2/commerce7.js"
       id="c7-javascript"
       data-tenant="young-inglewood-vineyards">
-    </Script> */}
+    </Script>
   </>
 }
 
